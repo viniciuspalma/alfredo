70671361f0b512c6e7cf810a446c126419a20253
viniciuspalma <vini_palma@outlook.com>
2015-03-10 01:39:59 -0300

    add main component to app

---
+++ scripts/components/alfredoApp.js
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+
+var React = require('React');
+
+var AlfredoApp = React.createClass({
+  render: function() {
+    return (
+      <div className="AlfredoApp">Hello</div>
+    );
+  }
+});
+
+module.exports = AlfredoApp;
